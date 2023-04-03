e0af9a0814713502a173f393780a72016f5b1315
Sabbir Hossen <sabbir.ms2000@gmail.com>
2023-04-03 12:14:10 +0600

    first commit

---
+++ arethmetic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int sabbir;
+    float sakib;
+    char sam;
+    scanf("%d %f %c", &sabbir, &sakib, &sam);
+    printf("%d,%0.1f,%c", sabbir, sakib, sam);
+    // printf("bangladesh");
+    return 0;
+}
